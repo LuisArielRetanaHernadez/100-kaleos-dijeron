929da6156ae005a2af89c05b329fb2980832e717
Luis Ariel Retana Hernandez <regalomessi10@gmail.com>
2026-09-19 15:21:34 -0600

    cors *

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ let initialization
 
 app.use(express.json())
 app.use(cors({
-  origin:  '*',
+  origin: process.env.CORS_ORIGIN || '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 }))
